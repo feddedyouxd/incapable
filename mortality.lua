dfa1b14d1ead4b37cb501971fe3e901124c55835
feddedyouxd <130783788+feddedyouxd@users.noreply.github.com>
2023-04-15 20:14:30 +0100

    Update mortality.lua

--- mortality.lua
+++ mortality.lua
@@ -5,94 +5,130 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
+    -- Set Center to true if you want the menu to appear in the center
+    -- Set AutoShow to true if you want the menu to appear when it is created
+    -- Position and Size are also valid options here
+    -- but you do not need to define them unless you are changing them :)
 
     Title = 'incapable#0001',
     Center = true, 
     AutoShow = true,
 })
 
+-- You do not have to set your tabs & groups up this way, just a prefrence.
 local Tabs = {
-    
+    -- Creates a new tab titled Main
     Main = Window:AddTab('Target'), 
     Visual = Window:AddTab('Visual'), 
     ['UI Settings'] = Window:AddTab('UI Settings'),
 }
 
+-- Groupbox and Tabbox inherit the same functions
+-- except Tabboxes you have to call the functions on a tab (Tabbox:AddTab(name))
 local LeftGroupBox = Tabs.Main:AddLeftGroupbox('Target')
 local RightGroupBox = Tabs.Main:AddRightGroupbox('Target Settings')
 
+-- Tabboxes are a tiny bit different, but here's a basic example:
+--[[
+
+local TabBox = Tabs.Main:AddLeftTabbox() -- Add Tabbox on left side
+
+local Tab1 = TabBox:AddTab('Tab 1')
+local Tab2 = TabBox:AddTab('Tab 2')
+
+-- You can now call AddToggle, etc on the tabs you added to the Tabbox
+]]
+
+-- Groupbox:AddToggle
+-- Arguments: Index, Options
+
+
 LeftGroupBox:AddToggle('Showdot', {
     Text = 'ShowBox',
-    Default = true, 
-    Tooltip = '', 
+    Default = true, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 });
+
 
 LeftGroupBox:AddToggle('Target', {
     Text = 'Target',
-    Default = true, 
-    Tooltip = '', 
+    Default = true, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 })
 Toggles.Target:OnChanged(function()
+    -- here we get our toggle object & then get its value
     getgenv().Target = Toggles.Target.Value
 end)
 
+-- This should print to the console: "My toggle state changed! New value: false"
 Toggles.Target:SetValue(false)
 
 LeftGroupBox:AddToggle('Airshot', {
     Text = 'Airshot',
-    Default = true, 
-    Tooltip = '', 
+    Default = true, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 })
 Toggles.Airshot:OnChanged(function()
+    -- here we get our toggle object & then get its value
     getgenv().AirshotFunccc = Toggles.Airshot.Value
 end)
 
+-- This should print to the console: "My toggle state changed! New value: false"
 Toggles.Airshot:SetValue(false)
+
 
 LeftGroupBox:AddToggle('NotifMode', {
     Text = 'Notification',
-    Default = true, 
+    Default = true, -- Default value (true / false)
 })
 
 LeftGroupBox:AddToggle('ChatMode', {
     Text = 'Chat Mode',
-    Default = true, 
-    Tooltip = '',
+    Default = true, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 })
-
 Toggles.ChatMode:OnChanged(function()
+    -- here we get our toggle object & then get its value
     getgenv().ChatMode = Toggles.ChatMode.Value
 end)
 
+-- This should print to the console: "My toggle state changed! New value: false"
 Toggles.ChatMode:SetValue(false)
+
+
 
 Toggles.NotifMode:OnChanged(function()
     getgenv().NotifMode = Toggles.NotifMode.Value
 end)
 
-
+-- This should print to the console: "My toggle state changed! New value: false"
 Toggles.NotifMode:SetValue(false)
 
 LeftGroupBox:AddToggle('AutoPred', {
     Text = 'Ping Based',
-    Default = true, 
-    Tooltip = '', 
+    Default = true, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 })
 Toggles.AutoPred:OnChanged(function()
+    -- here we get our toggle object & then get its value
     getgenv().AutoPrediction = Toggles.AutoPred.Value
 end)
 
+-- This should print to the console: "My toggle state changed! New value: false"
 Toggles.AutoPred:SetValue(false)
 
+-- Groupbox:AddInput
+-- Arguments: Idx, Info
 LeftGroupBox:AddInput('Prediction', {
     Default = '0.1229',
-    Numeric = false, 
-    Finished = false, 
+    Numeric = false, -- true / false, only allows numbers
+    Finished = false, -- true / false, only calls callback when you press enter
 
     Text = 'Prediction',
-    Tooltip = '', 
+    Tooltip = '', -- Information shown when you hover over the textbox
 
-    Placeholder = 'Enter New Pred Here', 
+    Placeholder = 'Enter New Pred Here', -- placeholder text when the box is empty
+    -- MaxLength is also an option which is the max length of the text
 })
 
 Options.Prediction:OnChanged(function()
@@ -101,12 +137,16 @@ end)
 
 Options.Prediction:SetValue(0.1229)
 
+-- Groupbox:AddDropdown
+-- Arguments: Idx, Info
+
 LeftGroupBox:AddDropdown('MyDropdown', {
     Values = { 'Head', 'UpperTorso', 'HumanoidRootPart', 'RightFoot' },
-    Default = 1, 
-    Multi = false, 
+    Default = 1, -- number index of the value / string
+    Multi = false, -- true / false, allows multiple choices to be selected
+
     Text = 'Hitpart',
-    Tooltip = 'This is a tooltip', 
+    Tooltip = 'This is a tooltip', -- Information shown when you hover over the textbox
 })
 
 Options.MyDropdown:OnChanged(function()
@@ -115,24 +155,36 @@ end)
 
 Options.MyDropdown:SetValue('UpperTorso')
 
+
 RightGroupBox:AddDivider()
 
--- targ sets :3
+-- // Target Settings -- //
+
 
 RightGroupBox:AddLabel('Hitbox Color'):AddColorPicker('ColorPicker', {
-    Default = Color3.new(0, 1, 0), 
-    Title = 'Hitbox Color', 
+    Default = Color3.new(0, 1, 0), -- Bright green
+    Title = 'Hitbox Color', -- Optional. Allows you to have a custom color picker title (when you open it)
 });
+
 
 RightGroupBox:AddSlider('MySlider', {
     Text = 'Hitbox Transparency',
+
+    -- Text, Default, Min, Max, Rounding must be specified.
+    -- Rounding is the number of decimal places for precision.
+
+    -- Example:
+    -- Rounding 0 - 5
+    -- Rounding 1 - 5.1
+    -- Rounding 2 - 5.15
+    -- Rounding 3 - 5.155
 
     Default = 0,
     Min = 0,
     Max = 1,
     Rounding = 1,
 
-    Compact = false, 
+    Compact = false, -- If set to true, then it will hide the label
 });
 
 Options.MySlider:SetValue(0.3)
@@ -141,13 +193,14 @@ RightGroupBox:AddDivider()
 
 RightGroupBox:AddToggle('FOVToggle', {
     Text = 'FOV',
-    Default = true, 
-    Tooltip = '', 
+    Default = true, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 });
 
 RightGroupBox:AddToggle('FOVFilled', {
     Text = 'FOV Filled',
-    Default = false, 
+    Default = false, -- Default value (true / false)
+    Tooltip = '', -- Information shown when you hover over the toggle
 });
 
 RightGroupBox:AddSlider('FOV', {
@@ -158,7 +211,7 @@ RightGroupBox:AddSlider('FOV', {
     Max = 750,
     Rounding = 0,
 
-    Compact = false, 
+    Compact = false, -- If set to true, then it will hide the label
 });
 
 Options.FOV:SetValue(280)
@@ -171,7 +224,7 @@ RightGroupBox:AddSlider('Thickness', {
     Max = 10,
     Rounding = 1,
 
-    Compact = false,
+    Compact = false, -- If set to true, then it will hide the label
 });
 
 Options.Thickness:SetValue(0)
@@ -179,54 +232,80 @@ Options.Thickness:SetValue(0)
 RightGroupBox:AddSlider('FOVTrans', {
     Text = 'FOV Transparency',
 
+    -- Text, Default, Min, Max, Rounding must be specified.
+    -- Rounding is the number of decimal places for precision.
+
+    -- Example:
+    -- Rounding 0 - 5
+    -- Rounding 1 - 5.1
+    -- Rounding 2 - 5.15
+    -- Rounding 3 - 5.155
+
     Default = 0.8,
     Min = 0,
     Max = 1,
     Rounding = 1,
 
-    Compact = false,
+    Compact = false, -- If set to true, then it will hide the label
 });
 
--- lib functions
 
+-- Library functions
+-- Sets the watermark visibility
 Library:SetWatermarkVisibility(true)
 
+-- Sets the watermark text
 Library:SetWatermark('incapable#0001')
 
-Library.KeybindFrame.Visible = true;
+Library.KeybindFrame.Visible = true; -- todo: add a function for this
 
 Library:OnUnload(function()
     print('Unloaded!')
     Library.Unloaded = true
 end)
 
--- UI
-
+-- UI Settings
 local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
 
+-- I set NoUI so it does not show up in the keybinds menu
 MenuGroup:AddButton('Unload', function() Library:Unload() end)
 MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' }) 
 
-Library.ToggleKeybind = Options.MenuKeybind
+Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
 
--- configs
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- ThemeManager (Allows you to have a menu theme system)
 
+-- Hand the library over to our managers
 ThemeManager:SetLibrary(Library)
 SaveManager:SetLibrary(Library)
 
+-- Ignore keys that are used by ThemeManager. 
+-- (we dont want configs to save themes, do we?)
 SaveManager:IgnoreThemeSettings() 
 
+-- Adds our MenuKeybind to the ignore list 
+-- (do you want each config to have a different menu key? probably not.)
 SaveManager:SetIgnoreIndexes({ 'MenuKeybind' }) 
 
+-- use case for doing it this way: 
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
 ThemeManager:SetFolder('MyScriptHub')
 SaveManager:SetFolder('MyScriptHub/specific-game')
 
+-- Builds our config menu on the right side of our tab
 SaveManager:BuildConfigSection(Tabs['UI Settings']) 
 
+-- Builds our theme menu (with plenty of built in themes) on the left side
+-- NOTE: you can also call ThemeManager:ApplyToGroupbox to add it to a specific groupbox
 ThemeManager:ApplyToTab(Tabs['UI Settings'])
 
---[[
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config 
+-- which has been marked to be one that auto loads!
 
+--[[
                                               
                            .'\                
                          //  ;               
@@ -254,8 +333,9 @@ ThemeManager:ApplyToTab(Tabs['UI Settings'])
         \     ).  .))            '    .      @incapable kike nigger chink faggot retard jew coon queef cum sex nut bum anal oralsex
      ----`-'-'  `''.::.________:::mx'' ---
 ]]
---tog
-    getgenv().ChatMode = falseimage.png
+
+-- Toggle
+    getgenv().ChatMode = false
         getgenv().PartMode = true
         getgenv().Key = Enum.KeyCode.Q
     --
